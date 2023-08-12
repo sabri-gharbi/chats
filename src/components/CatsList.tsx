b73e3c464d1e1a9083144d3cebb45d61e9a24567
Sabri Gharbi <sabrigharbi99@gmail.com>
2023-08-12 15:01:34 +0200

    feat: connect to mongo-db

--- src/components/CatsList.tsx
+++ src/components/CatsList.tsx
@@ -1,44 +1,40 @@
 import { useState } from "react";
 import CatCard from "./CatCard";
 import { api } from "~/utils/api";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import CatModal from "./CatModal";
 import { type Cat } from "~/Types";
 
 const CatsList = () => {
-  const { data: cats } = api.cats.all.useQuery();
+    const { mutate: createCat } = api.cats.create.useMutation();
 
-  const [selectedCat, setSelectedCat] = useState<Cat>();
+    const [selectedCat, setSelectedCat] = useState<Cat>();
 
-  const [catModalShown, setCatModalShown] = useState<boolean>(false);
+    const [catModalShown, setCatModalShown] = useState<boolean>(false);
 
-  const handleCloseCatModal = () => {
-    setCatModalShown(false);
-    setSelectedCat(undefined);
-  };
+    const handleCloseCatModal = () => {
+        setCatModalShown(false);
+        setSelectedCat(undefined);
+    };
 
-  const handleCatCardClick = (cat: Cat) => {
-    setSelectedCat(cat);
-    setCatModalShown(true);
-  };
+    const handleCatCardClick = (cat: Cat) => {
+        setSelectedCat(cat);
+        setCatModalShown(true);
+    };
 
-  return (
-    <>
-      {selectedCat && (
-        <CatModal
-          cat={selectedCat}
-          open={catModalShown}
-          onClose={handleCloseCatModal}
-        />
-      )}
+    return (
+        <>
+            {selectedCat && <CatModal cat={selectedCat} open={catModalShown} onClose={handleCloseCatModal} />}
 
-      <Box display="flex" flexDirection="column" gap={2}>
-        {(cats ?? []).map((cat) => (
-          <CatCard key={cat.id} cat={cat} onClick={handleCatCardClick} />
-        ))}
-      </Box>
-    </>
-  );
+            <Button onClick={() => createCat()}>Ceate a cat</Button>
+
+            {/* <Box display="flex" flexDirection="column" gap={2}>
+              {(cats ?? []).map((cat) => (
+                  <CatCard key={cat.id} cat={cat} onClick={handleCatCardClick} />
+              ))}
+          </Box> */}
+        </>
+    );
 };
 
 export default CatsList;
--- src/server/api/routers/cats/cats.ts
+++ src/server/api/routers/cats/cats.ts
@@ -1,8 +1,24 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { cats } from "./mock";
+import { prisma } from "~/server/db";
 
 export const catsRouter = createTRPCRouter({
-  all: publicProcedure.query(({}) => {
-    return cats;
-  }),
+    all: publicProcedure.query(({}) => {
+        return prisma.cat.findMany();
+    }),
+
+    create: publicProcedure.mutation(async () => {
+        await prisma.cat.create({
+            data: {
+                name: "Minou",
+                birthDate: new Date(2019, 5, 15),
+                breed: "Siamese",
+                gender: "Male",
+                city: "Paris",
+                description: "Minou is a playful and affectionate cat.",
+                photo: "https://api-ninjas.com/images/cats/abyssinian.jpg",
+                adoptionStatus: "Available",
+            },
+        });
+    }),
 });
